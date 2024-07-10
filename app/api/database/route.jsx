2ec0f4850196681476919073fb639db716a36d96
ingasafranova <sfrnv@yahoo.co.uk>
2024-07-10 15:05:17 +0100

    changed fetch cover

--- app/api/database/route.jsx
+++ app/api/database/route.jsx
@@ -18,35 +18,35 @@ export async function POST(req) {
     let bookData = body.formData;
     //console.log(bookData);
 
-    const queryParams = {
-      author: bookData.author,
-      title: bookData.title,
-      isbn: bookData.isbn,
-    };
-    const queryString = new URLSearchParams(queryParams).toString();
-    console.log(queryString);
-    const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${queryString}`
-    );
-    const book = await response.json();
-    //   const publisher = book.items[0]
-    console.log(book);
-    const coverUrl = book.items[0].volumeInfo.imageLinks.thumbnail;
-    console.log(coverUrl);
+    // const queryParams = {
+    //   author: bookData.author,
+    //   title: bookData.title,
+    //   isbn: bookData.isbn,
+    // };
+    // const queryString = new URLSearchParams(queryParams).toString();
+    // console.log(queryString);
+    // const response = await fetch(
+    //   `https://www.googleapis.com/books/v1/volumes?q=${queryString}`
+    // );
+    // const book = await response.json();
+    // //   const publisher = book.items[0]
+    // console.log(book);
+    // const coverUrl = book.items[0].volumeInfo.imageLinks.thumbnail;
+    // console.log(coverUrl);
 
     // const res = await fetch(
     //   `https://www.googleapis.com/books/v1/volumes?q=${bookId}`
     // );
     // const bookDetails = await res.json();
     // console.log(bookDetails)
-    // const isbnNumber = bookData.isbn;
+     const isbnNumber = bookData.isbn;
     // console.log(isbnNumber);
-    // let coverUrl;
-    // if (!isbnNumber) {
-    //   coverUrl = "";
-    // } else {
-    //   coverUrl = `https://covers.openlibrary.org/b/isbn/${isbnNumber}-M.jpg`;
-    // }
+    let coverUrl;
+    if (!isbnNumber) {
+      coverUrl = "";
+    } else {
+      coverUrl = `https://covers.openlibrary.org/b/isbn/${isbnNumber}-M.jpg`;
+    }
 
      bookData.cover = coverUrl;
     //console.log(bookData);
